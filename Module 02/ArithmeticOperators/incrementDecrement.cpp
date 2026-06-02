125171e59662b4fd3d551fb23bca25857294a0d0
vikash <vikashkelly@gmail.com>
2026-06-02 07:44:24 +0530

    Refactor increment and decrement examples for clarity

--- Module 02/ArithmeticOperators/incrementDecrement.cpp
+++ Module 02/ArithmeticOperators/incrementDecrement.cpp
@@ -4,9 +4,19 @@ int main()
 {
     // increment and decrement
     int x = 5;
-    ++x; //pre-increment -> x  x + 1
+    ++x; // pre-increment -> x  x + 1
     cout << x << endl;
-    x++; //post increment
-    
+    // x++; //post increment
+
+    // cout << ++x << endl;
+    cout << x++ << endl;
+
+    x--;
     cout << x << endl;
+    --x;
+    cout << x << endl;
+
+    /*
+    i am multi line comment 
+    */
 }
