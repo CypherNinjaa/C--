653ad95ed33b20cf5d8003a26f67ae0ccd7c0c2f
vikash <vikashkelly@gmail.com>
2026-06-02 08:54:25 +0530

    Add new examples for arithmetic operations: AreaOfCircle, BooleanDataType, CalculatePercentage, and ModulusOperator

---
+++ Module 02/ArithmeticOperators/BooleanDataType.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // bool flag = false;
+    // cout << flag;
+    cout << (5 < 3)<<endl;
+    cout << (4==4)<<endl;
+    cout << (4!=4)<<endl;
+}
---
+++ Module 02/ArithmeticOperators/ModulusOperator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout << 2 % 40;
+    // if a>b -> a%b -> reminder
+    // if a==b -> a%b -> 0
+    // if a<b -> a%b -> a
+    cout << -99 % -4;
+    // if a % b = a%(-b) = a % b
+    // if -a % b = -reminder
+    // if (-a) % (-b) = -a % b 
+    // due to hierarchy of operators like BODMAS
+}
