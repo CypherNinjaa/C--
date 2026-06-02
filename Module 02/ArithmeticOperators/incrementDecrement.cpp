5df75807ada6a4e2c48209a46dbb64ab26abae39
vikash <vikashkelly@gmail.com>
2026-06-02 07:18:00 +0530

    Add increment and decrement example in incrementDecrement.cpp

---
+++ Module 02/ArithmeticOperators/incrementDecrement.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // increment and decrement
+    int x = 5;
+    ++x; //pre-increment -> x  x + 1
+    cout << x << endl;
+    x++; //post increment
+    
+    cout << x << endl;
+}
