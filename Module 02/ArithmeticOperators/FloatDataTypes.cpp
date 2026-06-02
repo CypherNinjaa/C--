0725ab769bb0924b5a36ee8a6b1843634caac026
vikash <vikashkelly@gmail.com>
2026-06-02 07:59:52 +0530

    Add FloatDataTypes.cpp to demonstrate basic arithmetic operations

---
+++ Module 02/ArithmeticOperators/FloatDataTypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x = 5;
+    float y = 2;
+    cout << x / y << endl;
+    // cout << 5 / 2.0;
+
+    int a = 2.4;
+    cout << a;
+}
