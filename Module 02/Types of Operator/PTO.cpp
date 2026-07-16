1f57ea3d0536ab36e784a83ea480a7b89ec47244
vikash <vikashkelly@gmail.com>
2026-07-16 07:14:23 +0530

    Add initial implementations of PTO.cpp, operators.cpp, and tempCodeRunnerFile.cpp

---
+++ Module 02/Types of Operator/PTO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // bool p = false;
+    // bool q = false;
+    // bool r = true;
+    int p, q, r;
+    p = 1;
+    q = 2;
+    r = 2;
+    cout << ((p) == (q == r));
+}
