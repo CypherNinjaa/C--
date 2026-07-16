1e4c34c819468e9a0fdaa2cc56ebbe5fe2944e60
vikash <vikashkelly@gmail.com>
2026-07-16 06:40:42 +0530

    Refactor AreaOfCircle.cpp to take user input for radius and calculate area dynamically; add SumOfTwoNumber.cpp and cinSeInput.cpp for user input examples.

--- Module 02/ArithmeticOperators/AreaOfCircle.cpp
+++ Module 02/ArithmeticOperators/AreaOfCircle.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 int main()
 {
-    float r, pi, area;
-    pi = 3.1415;
-    r = 8;
-    area = pi * (r * r);
-    cout << area;
+    // float r, pi, area;
+    // pi = 3.1415;
+    // r = 8;
+    // area = pi * (r * r);
+    // cout << area;
+    float radius;
+    cout << "Enter the radius:";
+    cin >> radius;
+    float area = 3.1415 * radius * radius;
+    cout << "Area : "<<area<<endl;
 }
---
+++ Module 02/Taking Input From User/SumOfTwoNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "enter the 1st number: ";
+    cin >> x;
+    cout << "enter the 2nd number: ";
+    cin >> y;
+    cout << "sum of " << x << " and " << y << " is : " << x + y;
+
+    return 0;
+}
