ea229ef44cbd677f6601acc7c36b3890901d0766
vikash <vikashkelly@gmail.com>
2026-07-18 12:35:36 +0530

    Add EvenOdd and PrimeOrNot example programs

---
+++ Module 04/Questions (Brake and Continue)/PrimeOrNot.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    bool flag = true; // true means prime
+    for (int i = 2; i < n - 1; i++)
+    {
+        if (n % i == 0)
+        {
+
+            flag = false; // false means prime
+            break;
+        }
+    }
+    if (n == 1)
+    {
+        cout << "1 is neither prime nor composite";
+    }
+    else if (flag == true)
+    {
+        cout << "it is a Prime number" << endl;
+    }
+    else
+    {
+        cout << "it is a composite number" << endl;
+    }
+}
