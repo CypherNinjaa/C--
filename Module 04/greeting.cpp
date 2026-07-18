cfdfe933be213e1aab70590b246a1434abbcf68a
vikash <vikashkelly@gmail.com>
2026-07-18 10:31:02 +0530

    Add new C++ programs: EvenNumber1to100, NTimesHelloWorld, PrintNumber, TableOf19, and greeting

---
+++ Module 04/greeting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // 10 times good morning;
+    // initialization condition increment
+    for (int i = 0; i   <= 03;    i++)
+    {
+        cout << "Good Morning" << endl;
+    }
+}
