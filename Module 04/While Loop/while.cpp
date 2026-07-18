d4d237ff9fa7500c84d7e2b6ebf32de30dfb9d84
vikash <vikashkelly@gmail.com>
2026-07-18 11:53:39 +0530

    Add while loop example program to demonstrate basic iteration in C++

---
+++ Module 04/While Loop/while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    // for (; i <= 10;)
+    // {
+    //     cout << i << " ";
+    //     i++;
+    // }
+
+    while (i <= 10)
+    {
+        cout << i << " ";
+        i++;
+    }
+}
