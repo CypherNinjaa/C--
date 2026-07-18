9a10187a75fc88bc81050c6bf88c117173412d62
vikash <vikashkelly@gmail.com>
2026-07-18 12:07:58 +0530

    Add while and do-while loop example programs

--- Module 04/While and DoWhile Loop/DowhileLoop.cpp
+++ Module 04/While and DoWhile Loop/DowhileLoop.cpp
@@ -3,15 +3,16 @@ using namespace std;
 int main()
 {
     int i = 1;
+    // int i; -> garbage value
     // for (; i <= 10;)
     // {
     //     cout << i << " ";
     //     i++;
     // }
 
-    while (i <= 10)
+    do
     {
         cout << i << " ";
         i++;
-    }
+    } while (i <= 10);
 }
---
+++ Module 04/While and DoWhile Loop/while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i =10;
+    // int i; -> garbage value
+    // for (; i <= 10;)
+    // {
+    //     cout << i << " ";
+    //     i++;
+    // }
+
+    while (i <= 10)
+    {
+        cout << i << " ";
+        i++;
+    }
+}
