fbce968bea69f8cadf6db681173681cc531b3db6
vikash <vikashkelly@gmail.com>
2026-07-19 08:49:01 +0530

    Add infiniteloop and pto example programs

---
+++ Module 04/infiniteloop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // while ('a'<'b')
+    // {
+    //     cout<<"Vikash ";
+    // }
+    int x = 4,y=0,z;
+    while (x>=0)
+    {
+        x--;
+        y++;
+        if (x==y)
+        {
+            continue;
+        }else{
+            cout<<x<<" "<<y<<endl; 
+        }
+        
+    }
+    
+    
+}
